bb0ec29eb2ab73df84234ae54f41bdba9dc81113
Jeffrey Li <jeffrey@vectorwyse.com>
2021-03-24 12:07:57 -0400

    make finder optional

--- src/helpers.php
+++ src/helpers.php
@@ -5,12 +5,17 @@ namespace Vectorwyse\PhpStyles;
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-function styles(Finder $finder, array $rules = []): Config
+function styles(Finder $finder = null, array $rules = []): Config
 {
     $rules = array_merge(require __DIR__.'/rules.php', $rules);
 
-    return Config::create()
-                ->setFinder($finder)
+    $config = Config::create()
                 ->setRiskyAllowed(true)
                 ->setRules($rules);
+    
+    if ($finder) {
+        $config = $config->setFinder($finder);
+    }
+
+    return $config;
 }
